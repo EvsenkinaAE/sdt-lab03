c7d1ab8e9ec127c3503958e4e944e1a94f16dfda
u211-11 <u211-11@public.mpei.local>
2018-05-29 09:56:15 +0300

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,71 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
-#include <cassert>
-#include "Temperature.h"
 using namespace std;
-
-    int main() {
-        test_temperature_imput ();
-
-
-        size_t number;
-        cout<<" Vvedite kolichestvo znacheniy";
-        cin>>number;
-        size_t stolbec;
-        cout<<"\n Vvedite kolishestvo stolbtsov";
-        cin>>stolbec;
-        vector <Temperature> X (number);
-        cout<<"\n Vvod massiva";
-        for(int i=0;i<number;i++)
+int main() {
+    size_t number;
+    cerr<<" Vvedite kolichestvo znacheniy";
+            cin>>number;
+    size_t stolbec;
+    cerr<<"\n Vvedite kolishestvo stolbtsov";
+    cin>>stolbec;
+    vector <double> X (number);
+    cerr<<"\n Vvod massiva";
+    for(int i=0;i<number;i++)
+    {
+        cin>>X[i];
+    }
+    double min,max;
+    min=X[0];
+    max=X[0];
+    for(double x:X) {
+        if (x > max)
+            max = x;
+        else if (x < min)
+            min = x;
+    }
+    cerr<<"\n max="<<max<<"   min="<<min<<"\n";
+    vector<int>col_count(stolbec);
+    for(double x:X)
+    {
+    size_t index= (size_t)((x-min)/(max-min)*stolbec);
+        if(x==max) {
+            index = index - 1;
+        }
+        col_count[index]=col_count[index]+1;
+    }
+    for(int i=0;i<stolbec;i++)
+    {
+        cout<< col_count[i] << "|";
+        for (int j = 0; j<col_count[i]; j++)
         {
-            cin>>X[i];
-        }
-        double min,max;
-        min=X[0].temp;
-        max=X[0].temp;
-        for(Temperature x:X) {
-            if (x.temp > max)
-                max = x.temp;
-            else if (x.temp < min)
-                min = x.temp;
-        }
-        cout<<"\n max="<<max<<"   min="<<min<<"\n";
-        vector<int>col_count(stolbec);
-        for(Temperature x:X)
-        {
-            size_t index= (size_t)((x-min)/(max-min)*stolbec);
-            if(x==max) {
-                index = index - 1;
-            }
-            col_count[index]=col_count[index]+1;
-        }
-        float maxcol_count;
-        double kof;
-        maxcol_count=col_count[0];
-        for(float x:col_count)
-        {
-            if(maxcol_count<x)
-                maxcol_count=x;
-        }
-        kof=1;
-        if(maxcol_count>shirina)
-            kof=(double)(1-(maxcol_count-shirina)/(maxcol_count));
-
-        for(int i=0;i<stolbec;i++)
-        {   if (col_count[i]>=100)
-                cout<< col_count[i] << "|";
-            else
-            if (col_count[i]<10)
-                cout<<"  "<< col_count[i] << "|";
-            else
-                cout<<" "<<col_count[i] << "|";
-            for (int j = 0; j<col_count[i]*kof; j++)
-            {
-                cout << "*";
-            }
-            cout<<endl;
-        }
+            cout << "*";
+    }
+        cout<<endl;
+    }
 
     return 0;
 }
